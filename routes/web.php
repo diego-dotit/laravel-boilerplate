d6ec6fc49647eea5bd6b428920ab62b6f46a8d8a
Diego <enache.cristiann@gmail.com>
2025-10-16 22:38:46 +0300

    diverse fixes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Livewire\Actions\Logout;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -9,9 +8,6 @@ Route::get('/', function () {
 })->name('home');
 
 Route::redirect('login', 'admin/login');
-
-Route::middleware(['auth'])->get('logout', Logout::class)->name('logout');
-
 
 Route::prefix('admin')
     ->name('admin.')
@@ -25,25 +21,7 @@ Route::prefix('admin')
             Volt::route('users', 'admin.users.index')->name('users');
             Volt::route('user/create', 'admin.users.form')->name('users.create');
             Volt::route('user/{user}/edit', 'admin.users.form')->name('users.edit');
-            Volt::route('user/{user}', 'admin.users.show')->name('users.show');
+            // Volt::route('user/{user}', 'admin.users.show')->name('users.show');
         });
     });
 
-// Route::middleware(['auth'])->group(function () {
-//     Route::redirect('settings', 'settings/profile');
-
-//     Volt::route('settings/profile', 'settings.profile')->name('profile.edit');
-//     Volt::route('settings/password', 'settings.password')->name('password.edit');
-//     Volt::route('settings/appearance', 'settings.appearance')->name('appearance.edit');
-
-//     Volt::route('settings/two-factor', 'settings.two-factor')
-//         ->middleware(
-//             when(
-//                 Features::canManageTwoFactorAuthentication()
-//                     && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-//                 ['password.confirm'],
-//                 [],
-//             ),
-//         )
-//         ->name('two-factor.show');
-// });
